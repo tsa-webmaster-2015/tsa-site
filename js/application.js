04655e4ed6a35b9aded25f12cd017134b6f8b641
ekyl <nkdigiulio@gmail.com>
2016-01-31 00:04:06 -0700

    attempt to copy over the same URL changer, this is important so we can link between individual pages to meet requirements

--- js/application.js
+++ js/application.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
-    changePage('home');
-
-    $('.navitem').click(function () {
-        changePage(this.id);
+    $('nav').hide().fadeIn();
+    var page = location.search.substring(1, 2000);
+    if (page) {
+        $('#content').load('/pages/' + page + '.html');
+    } else {
+        $('#content').load('/pages/home.html');
+    }
+    $('#content').fadeIn();
+    $('nav').find('span').click(function () {
+        id = this.id
+        $('#content').load('/partials/' + id + '.html').hide().fadeIn();
+        window.history.pushState("string", "Title", "/?" + id);
     });
 
-    function changePage(id) {
-        $('#content').load('pages/' + id + ".html").hide().fadeIn();
-        window.scrollTo(0, 0);
-    }
 
     $(window).scroll(function () {
         var ht = $('.block').parent().children('.txt').height();
